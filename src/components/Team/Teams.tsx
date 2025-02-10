91bf8ec9570b5f7f8dfd9d023e3bf8a103609c28
Zaibhere1 <zaibchaudhary4492@gmail.com>
2025-02-10 13:44:17 +0500

    teams mobile view

--- src/components/Team/Teams.tsx
+++ src/components/Team/Teams.tsx
@@ -1,27 +1,12 @@
-import { For, onMount } from "solid-js";
+import { For } from "solid-js";
 import { FaBrandsLinkedin } from "solid-icons/fa";
 import { A } from "@solidjs/router";
 import { teams } from "../../services/Services";
-import Swiper from "swiper";
 import "swiper/css";
 import "swiper/css/scrollbar";
 import "swiper/css/free-mode";
-import { FreeMode, Scrollbar } from "swiper/modules";
 
 const Teams = () => {
-  let swiperRef;
-
-  onMount(() => {
-    new Swiper(swiperRef, {
-      direction: "horizontal",
-      slidesPerView: 2,
-      spaceBetween: 20,
-      freeMode: true,
-      scrollbar: { draggable: true },
-      modules: [FreeMode, Scrollbar],
-    });
-  });
-
   return (
     <div>
       <div>
@@ -32,31 +17,29 @@ const Teams = () => {
 
       {/* Vertical Swiper for Mobile */}
       <div class="block md:hidden">
-        <div ref={swiperRef} class="swiper h-[500px]">
-          <div class="swiper-wrapper">
-            <For each={teams}>
-              {(team) => (
-                <div class="swiper-slide w-full">
-                  <div class="dark:text-white shadow-lg rounded-lg overflow-hidden hover:scale-105 duration-500">
-                    <img src={team.pic} alt="Team Member" class="px-20 mt-10" />
-                    <p class="relative md:text-xl md:font-bold font-bold text-xl text-center my-2">
-                      {team.name}
-                    </p>
-                    <p class="mb-2 text-sm text-center dark:bg-white dark:text-black">
-                      {team.designation}
-                    </p>
-                    <span class="md:text-4xl mb-45 text-2xl flex justify-end">
-                      <A href={team.url}>
-                        <FaBrandsLinkedin class="hover:text-blue-600 hover:scale-125 duration-1000" />
-                      </A>
-                    </span>
-                  </div>
+        <div class="carousel rounded-lg w-full px-2">
+          <For each={teams}>
+            {(team) => (
+              <div class="carousel-item w-full">
+                <div class="dark:text-white shadow-lg rounded-lg overflow-hidden hover:scale-105 duration-500">
+                  <img src={team.pic} alt="Team Member" class="px-20 mt-10" />
+                  <p class="relative md:text-xl md:font-bold font-bold text-xl text-center my-2">
+                    {team.name}
+                  </p>
+                  <p class="mb-2 text-sm text-center dark:bg-white dark:text-black">
+                    {team.designation}
+                  </p>
+                  <span class="md:text-4xl mb-45 text-2xl flex justify-end">
+                    <A href={team.url}>
+                      <FaBrandsLinkedin class="hover:text-blue-600 hover:scale-125 duration-1000" />
+                    </A>
+                  </span>
                 </div>
-              )}
-            </For>
-          </div>
-          <div class="swiper-scrollbar"></div>
+              </div>
+            )}
+          </For>
         </div>
+        <div class="swiper-scrollbar"></div>
       </div>
 
       {/* Grid for Larger Screens */}
--- src/components/ourservices/OurServices.tsx
+++ src/components/ourservices/OurServices.tsx
@@ -9,7 +9,7 @@ const OurServices = () => {
           Our Services{" "}
         </button>
       </div>
-      <div class="grid md:grid-cols-3 gap-7 grid-cols-2 dark:text-white justify-center pl-24">
+      <div class="grid md:grid-cols-3 gap-7 grid-cols-2 dark:text-white justify-center md:pl-24">
         <For each={blogPosts}>
           {(blog) => (
             <div class="flex flex-col justify-center max-w-sm rounded overflow-hidden shadow-lg min-h-[350px]">
@@ -28,7 +28,7 @@ const OurServices = () => {
                   </p>
                 </div>
                 <div class="px-6 py-4 mt-auto text-center">
-                  <button class="text-lg font-bold text-white bg-gradient-to-l from-cyan-600 to-blue-600 hover:bg-cyan-700 duration-1000 md:text-lg md:px-2 md:py-1 rounded-lg hover:scale-110 px-4 py-2">
+                  <button class="text-sm font-bold text-white bg-gradient-to-l from-cyan-600 to-blue-600 hover:bg-cyan-700 duration-1000 md:text-lg md:px-2 md:py-1 rounded-lg hover:scale-110 px-1 py-1">
                     Learn More {"->"}
                   </button>
                 </div>
